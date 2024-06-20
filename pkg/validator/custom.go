e319f73f059ba90d6c429ef9d213c2ca318d07ab
MovingTowardsADream <smirnov.matveychik@mail.ru>
2024-06-20 17:09:37 +0300

    validator

--- pkg/validator/custom.go
+++ pkg/validator/custom.go
@@ -3,7 +3,6 @@ package custom_validator
 import (
 	"fmt"
 	"github.com/go-playground/validator/v10"
-	"reflect"
 )
 
 type CustomValidator struct {
@@ -23,54 +22,7 @@ func (cv *CustomValidator) ValidateStruct(obj interface{}) error {
 	if err != nil {
 		fieldErr := err.(validator.ValidationErrors)[0]
 
-		return cv.newValidationError(fieldErr.Field(), fieldErr.Value(), fieldErr.Tag(), fieldErr.Param())
+		return fmt.Errorf(fieldErr.Field(), fieldErr.Value(), fieldErr.Tag(), fieldErr.Param())
 	}
 	return nil
-}
-
-func (cv *CustomValidator) newValidationError(field string, value interface{}, tag string, param string) error {
-	switch tag {
-	case "required":
-		return fmt.Errorf("field %s is required", field)
-	case "email":
-		return fmt.Errorf("field %s must be a valid email address", field)
-	case "password":
-		return cv.passwdErr
-	case "min":
-		return fmt.Errorf("field %s must be at least %s characters", field, param)
-	case "max":
-		return fmt.Errorf("field %s must be at most %s characters", field, param)
-	default:
-		return fmt.Errorf("field %s is invalid", field)
-	}
-}
-
-func (cv *CustomValidator) mailValidate(fl validator.FieldLevel) bool {
-
-	if fl.Field().Kind() != reflect.String {
-		cv.passwdErr = fmt.Errorf("field %s must be a string", fl.FieldName())
-		return false
-	}
-
-	// get the value of the field
-	fieldValue := fl.Field().String()
-
-	_ = fieldValue
-
-	return true
-}
-
-func (cv *CustomValidator) phoneValidate(fl validator.FieldLevel) bool {
-
-	if fl.Field().Kind() != reflect.String {
-		cv.passwdErr = fmt.Errorf("field %s must be a string", fl.FieldName())
-		return false
-	}
-
-	// get the value of the field
-	fieldValue := fl.Field().String()
-
-	_ = fieldValue
-
-	return true
 }
