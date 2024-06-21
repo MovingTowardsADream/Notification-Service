fd4f40dfdd85c9ff31ebb721b4b3af9c87fcc743
MovingTowardsADream <smirnov.matveychik@mail.ru>
2024-06-21 18:32:51 +0300

    testing

---
+++ tests/suite/suite.go
@@ -0,0 +1,48 @@
+package suite
+
+import (
+	"Notification_Service/configs"
+	notifyv1 "Notification_Service/protos/gen/go/notify"
+	"context"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+	"net"
+	"testing"
+)
+
+const _default_gRPC_Host = "localhost"
+
+type Suite struct {
+	*testing.T
+	Cfg         *configs.Config
+	UsersClient notifyv1.UsersClient
+}
+
+func New(t *testing.T) (context.Context, *Suite) {
+	t.Helper()
+	t.Parallel()
+
+	cfg := configs.MustLoad()
+
+	ctx, cancel := context.WithTimeout(context.Background(), cfg.GRPC.Timeout)
+
+	// Called when the context terminates
+	t.Cleanup(func() {
+		t.Helper()
+		cancel()
+	})
+
+	cc, err := grpc.DialContext(context.Background(),
+		net.JoinHostPort(_default_gRPC_Host, cfg.Port),
+		grpc.WithTransportCredentials(insecure.NewCredentials()))
+
+	if err != nil {
+		t.Fatalf("grpc server connection failed: %v", err)
+	}
+
+	return ctx, &Suite{
+		T:           t,
+		Cfg:         cfg,
+		UsersClient: notifyv1.NewUsersClient(cc),
+	}
+}
