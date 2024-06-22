d09813ceae5af35829206d4a31e9fbff82a36305
MovingTowardsADream <smirnov.matveychik@mail.ru>
2024-06-22 17:56:30 +0300

    testing users preferences

--- tests/users/users_preferences_test.go
+++ tests/users/users_preferences_test.go
@@ -42,7 +42,54 @@ func TestUsersPreferences_NotFound(t *testing.T) {
 		},
 	})
 
+	require.Error(t, err)
 	require.ErrorContains(t, err, "object not found")
 }
 
-// TODO Testing invalid arguments in table driven tests
+func TestUsersPreferences_InvalidArgument(t *testing.T) {
+	ctx, st := suite.New(t)
+
+	tests := []struct {
+		name        string
+		userId      string
+		preferences *notifyv1.Preferences
+		expectedErr string
+	}{
+		{
+			name:   "Request with empty UserId",
+			userId: "",
+			preferences: &notifyv1.Preferences{
+				Mail: &notifyv1.MailApproval{
+					Approval: true,
+				},
+				Phone: &notifyv1.PhoneApproval{
+					Approval: true,
+				},
+			},
+			expectedErr: "field UserId is required",
+		},
+		{
+			name:        "Request with empty Preferences",
+			userId:      "c3e72e9a467a8f4d327fyc6ba1c66e7u",
+			preferences: nil,
+			expectedErr: "field Preferences is required",
+		},
+		{
+			name:        "Request with empty UserId and Preferences",
+			userId:      "",
+			preferences: nil,
+			expectedErr: "field UserId is required",
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			_, err := st.UsersClient.UserPreferences(ctx, &notifyv1.UserPreferencesRequest{
+				UserId:      tt.userId,
+				Preferences: tt.preferences,
+			})
+			require.Error(t, err)
+			require.Contains(t, err.Error(), tt.expectedErr)
+		})
+	}
+}
